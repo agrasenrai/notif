b9f0af91d0686a1198c842a14e41a479a29129cf
agrasenrai <agrasenrai@gmail.com>
2025-12-14 20:05:39 +0530

    update16

---
+++ sw.js
@@ -0,0 +1,16 @@
+// Service Worker for notifications
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
+
